14ee1a445b56f909020bbdc65c7f78e9b5124091
unknown <876700669@qq.com>
2018-09-11 22:48:38 +0800

    finish 1.4

---
+++ homework1/homework1/program1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace homework1
+{
+    class program1
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("a: ");
+
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}", a);
+
+            Console.WriteLine("b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}", b);
+
+            Console.WriteLine("a * b == {0} ", a * b);
+           
+
+            
+        }
+    }
+}
---
+++ homework1/program2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace program2
+{
+    static class Program
+    {
+        public class Form1 : Form
+        {
+            TextBox txt1 = new TextBox();
+            TextBox txt2 = new TextBox();
+            Button btn = new Button();
+            Label lbl = new Label();
+
+            public void init()
+            {
+                this.Controls.Add(txt1);
+                this.Controls.Add(txt2);
+                this.Controls.Add(btn);
+                this.Controls.Add(lbl);
+
+                txt1.Dock = System.Windows.Forms.DockStyle.Left;
+                txt2.Dock = System.Windows.Forms.DockStyle.Right;
+                lbl.Dock = System.Windows.Forms.DockStyle.Bottom;
+                btn.Dock = System.Windows.Forms.DockStyle.Fill;
+                btn.Text = "计算两数的乘积";
+                lbl.Text = "结果为";
+                this.Size = new Size(500,500);
+
+                btn.Click += new System.EventHandler(this.button_Click);
+
+            }
+            public void button_Click(object sender, EventArgs e)
+            {
+
+                string s1 = txt1.Text;
+                string s2 = txt2.Text;
+                double x = double.Parse(s1);
+                double y = double.Parse(s2);
+                double xy = x * y;
+                lbl.Text = x + "和" + y + "的乘积为" + xy;
+            }
+        }
+        
+
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Form1 f = new Form1();
+            f.Text = "multiply";
+            f.init();
+            
+            Application.Run(f);
+        }
+    }
+}
