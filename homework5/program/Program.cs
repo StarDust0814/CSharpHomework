59b05a02da664bf6827a8c765622535017353190
unknown <876700669@qq.com>
2018-10-17 19:37:21 +0800

    finish cayleyTree and linq

---
+++ homework5/program/Program.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace program1
+{
+    //订单类，包含订单号、用户名、多条订单明细
+    class Order
+    {
+        private int _orderNumber;
+        private string _guestName;
+        OrderDetail[] _orderDetails;
+        public Order(int orderNumber, string guestName, OrderDetail[] orderDetails)
+        {
+            this._orderNumber = orderNumber;
+            this._guestName = guestName;
+            this._orderDetails = orderDetails;
+        }
+        public string guestName
+        {
+            get { return _guestName; }
+        }
+        public int orderNumber
+        {
+            get { return _orderNumber; }
+        }
+        public OrderDetail[] orderDetail
+        {
+            get { return _orderDetails; }
+        }
+        //根据订单明细计算订单的总金额
+        public double SumPrice(OrderDetail[] o)
+        {
+            double sum = 0;
+            for (int b = 0; b < o.Length; b++)
+            {
+                sum += o[b].sumPrice();
+            }
+            return sum;
+        }
+    }
+    //订单明细，即一个用户所需的所有商品、数量、单价
+    class OrderDetail
+    {
+        private string _goodName;
+        private int _goodNumber;
+        private double _singlePrice;
+        public OrderDetail(string goodName, int goodNumber, double singlePrice)
+        {
+            this._goodName = goodName;
+            this._goodNumber = goodNumber;
+            this._singlePrice = singlePrice;
+        }
+        public string goodName
+        {
+            get { return _goodName; }
+        }
+        public int goodNumber
+        {
+            get { return _goodNumber; }
+        }
+        public double singlePrice
+        {
+            get { return _singlePrice; }
+        }
+        //获取订单明细的总金额
+        public double sumPrice()
+        {
+            return _goodNumber * _singlePrice;
+        }
+    }
+    //订单服务
+    class OrderService
+    {
+        //存储所有订单数据
+        public static List<Order> orders = new List<Order>();
+        //展示订单信息
+        public static void showOrder()
+        {
+            foreach (Order o in orders)
+            {
+                int orderDetailLength = o.orderDetail.Length;
+                Console.WriteLine(o.orderNumber + "   " + o.guestName);
+                for (int count = 0; count < orderDetailLength; count++)
+                {
+                    Console.WriteLine(o.orderDetail[count].goodName + "   " + o.orderDetail[count].goodNumber + "   " + o.orderDetail[count].singlePrice);
+                }
+                Console.Write("\n"); ;
+            }
+        }
+        //删除订单
+        public static void removeOrder(int orderNumber)
+        {
+            bool have = false;//判断是否有该订单号的订单
+            foreach (Order o in orders)
+            {
+                if (o.orderNumber == orderNumber)
+                {
+                    have = true;
+                    orders.Remove(o);
+                    break;
+                }
+            }
+            if (have == false)
+            {
+                throw new MyOrderException("The order doesn't exit, you don't need to remove it.", "remove wrong");
+            }
+        }
+        //修改订单内容
+        public static void changeOrders(int changeNumber, int orderNumber, string guestName, OrderDetail[] orderDetails)
+        {
+            bool exit = false;//判断是否有该订单号的订单
+            foreach (Order o in orders)
+            {
+                if (o.orderNumber == changeNumber)
+                {
+                    exit = true;
+                    orders[changeNumber - 1] = new Order(orderNumber, guestName, orderDetails);
+                    break;
+                }
+            }
+            if (exit == false) { throw new MyOrderException("The order doesn't exit, you have to input new one.", "change wrong"); }
+        }
+        //通过订单号查询订单
+        public static void findOrderByOrderNumber(int orderNumber)
+        {
+            bool have = false;//判断是否有该订单号的订单
+            foreach (Order o in orders)
+            {
+                if (o.orderNumber == orderNumber)
+                {
+                    have = true;
+                    int orderDetailLength = o.orderDetail.Length;
+                    Console.WriteLine(o.orderNumber + "   " + o.guestName);
+                    for (int count = 0; count < orderDetailLength; count++)
+                    {
+                        Console.WriteLine(o.orderDetail[count].goodName + "   " + o.orderDetail[count].goodNumber + "   " + o.orderDetail[count].singlePrice);
+                    }
+                    break;
+                }
+            }
+            if (have == false)
+            {
+                throw new MyOrderException("The order doesn't exit, you have to input a new ordernumber.", "findByNumber wrong");
+            }
+        }
+        //通过商品名查询订单
+        public static void findOrderByGoodName(string goodName)
+        {
+            bool have = false;
+            foreach (Order o in orders)
+            {
+                int orderDetailLength = o.orderDetail.Length;
+                for (int count = 0; count < orderDetailLength; count++)
+                {
+                    if (o.orderDetail[count].goodName == goodName)
+                    {
+                        have = true;
+                        Console.WriteLine(o.orderNumber + "   " + o.guestName);
+                        Console.WriteLine(o.orderDetail[count].goodName + "   " + o.orderDetail[count].goodNumber + "   " + o.orderDetail[count].singlePrice);
+                    }
+                }
+            }
+            if (have == false) { throw new MyOrderException("The order doesn't exit, you have to input a new goodname.", "findByName wrong"); }
+        }
+        //通过用户名查询订单
+        public static void findOrderByGuestName(string guestName)
+        {
+            bool have = false;
+            foreach (Order o in orders)
+            {
+                if (o.guestName == guestName)
+                {
+                    have = true;
+                    int orderDetailLength = o.orderDetail.Length;
+                    Console.WriteLine(o.orderNumber + "   " + o.guestName);
+                    for (int count = 0; count < orderDetailLength; count++)
+                    {
+                        Console.WriteLine(o.orderDetail[count].goodName + "   " + o.orderDetail[count].goodNumber + "   " + o.orderDetail[count].singlePrice);
+                    }
+                    break;
+                }
+            }
+            if (have == false)
+            {
+                throw new MyOrderException("The order doesn't exit, you have to input a new guestName.", "findByGuest wrong");
+            }
+        }
+    }
+    //异常处理类
+    public class MyOrderException : ApplicationException
+    {
+        private string information;
+        public MyOrderException(string message, string information) : base(message)
+        {
+            this.information = information;
+        }
+        public string getInformation()
+        {
+            return information;
+        }
+    }
+    class program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to use the order system!");
+            OrderDetail detail1OfA = new OrderDetail("A", 2, 1000);
+            OrderDetail detail2OfA = new OrderDetail("A", 1, 1000);
+            OrderDetail detail3OfA = new OrderDetail("A", 3, 1000);
+            OrderDetail detail1OfB = new OrderDetail("B", 3, 1500);
+            OrderDetail detail2OfB = new OrderDetail("B", 2, 1500);
+            OrderDetail detail3OfB = new OrderDetail("B", 3, 1500);
+            OrderDetail detail1OfC = new OrderDetail("C", 3, 2000);
+            OrderDetail detail2OfC = new OrderDetail("C", 2, 2000);
+            OrderDetail detail3OfC = new OrderDetail("C", 1, 2000);
+            OrderDetail[] orderDetail1 = { detail1OfA, detail1OfB, detail1OfC };
+            Order order1 = new Order(1, "Frank", orderDetail1);
+            OrderService.orders.Add(order1);
+            OrderService.showOrder();
+            Console.WriteLine("Add");
+            OrderDetail[] orderDetail2 = { detail2OfA, detail2OfB, detail2OfC };
+            OrderDetail[] orderDetail3 = { detail3OfA, detail3OfB, detail3OfC };
+            Order order2 = new Order(2, "Jack", orderDetail2);
+            Order order3 = new Order(3, "William", orderDetail2);
+            OrderService.orders.Add(order2);
+            OrderService.showOrder();
+            Order[] orders = { order1, order2, order3 };
+            Console.WriteLine('\n');
+            //使用linq语句按照订单金额检索
+            Console.WriteLine("Find by sumPrice > 10000: ");
+            double[] ordersSumPrice = { order1.SumPrice(orderDetail1), order2.SumPrice(orderDetail2), order3.SumPrice(orderDetail3) };
+            var sumPriceSelected = from n in orders where n.SumPrice(n.orderDetail) > 10000 select n;
+            foreach (var i in sumPriceSelected)
+            {
+                int orderDetailLength = i.orderDetail.Length;
+                int count;
+                Console.WriteLine(i.orderNumber + "   " + i.guestName);
+                for (count = 0; count < orderDetailLength; count++)
+                {
+                    Console.WriteLine(i.orderDetail[count].goodName + "   " + i.orderDetail[count].goodNumber + "   " + i.orderDetail[count].singlePrice);
+                }
+                Console.WriteLine('\n');
+            }
+            Console.WriteLine('\n');
+            //使用linq语句按照商品名称检索
+            Console.WriteLine("Find by goodName: ");
+            var goodnameSelected = from m in orders where m.orderDetail[0].goodName == "A" select m;
+            foreach (var i in goodnameSelected)
+            {
+                int orderDetailLength = i.orderDetail.Length;
+                int count;
+                Console.WriteLine(i.orderNumber + "   " + i.guestName);
+                for (count = 0; count < orderDetailLength; count++)
+                {
+                    Console.WriteLine(i.orderDetail[count].goodName + "   " + i.orderDetail[count].goodNumber + "   " + i.orderDetail[count].singlePrice);
+                }
+                Console.WriteLine('\n');
+            }
+            Console.WriteLine('\n');
+            //使用Linq语句按照客户字段检索
+            Console.WriteLine("Find by guestName: ");
+            var guestnameSelected = from a in orders where a.guestName == "Jack" select a;
+            foreach (var i in guestnameSelected)
+            {
+                int orderDetailLength = i.orderDetail.Length;
+                int count;
+                Console.WriteLine(i.orderNumber + "   " + i.guestName);
+                for (count = 0; count < orderDetailLength; count++)
+                {
+                    Console.WriteLine(i.orderDetail[count].goodName + "   " + i.orderDetail[count].goodNumber + "   " + i.orderDetail[count].singlePrice);
+                }
+                Console.WriteLine('\n');
+            }
+
+            Console.Write("Done!!!");
+        }
+    }
+}
+
+
---
+++ homework5/program2/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        //输入CayleyTree的角度和长度
+        TextBox angle1 = new TextBox() { Location = new Point(500, 20) };
+        TextBox angle2 = new TextBox() { Location = new Point(500, 40) };
+        TextBox length1 = new TextBox() { Location = new Point(500, 80) };
+        TextBox length2 = new TextBox() { Location = new Point(500, 100) };
+        Label labelOfAngle = new Label() { Width = 150, Location = new Point(500, 0) };
+        Label labelOfLength = new Label() {Width=150, Location = new Point(500, 60) };
+
+        public void init()
+        {
+            //添加控件
+            VScrollBar vbar;
+            vbar = new VScrollBar() { Location = new Point(300, 80) };
+            vbar.Parent = this;
+            vbar.Minimum = 0;
+            vbar.Maximum = 100;
+            vbar.SmallChange = 1;
+            vbar.LargeChange = 10;
+            vbar.Value = (vbar.Maximum - vbar.Minimum) / 2;
+            vbar.ValueChanged += new EventHandler(vbar_OnValueChanged);
+            this.Controls.Add(angle1);
+            this.Controls.Add(angle2);
+            this.Controls.Add(length1);
+            this.Controls.Add(length2);
+            this.Controls.Add(labelOfLength);
+            this.Controls.Add(labelOfAngle);
+            this.Controls.Add(vbar);
+            labelOfAngle.Text = "请输入两个角度： ";
+            labelOfLength.Text = "请输入两个长度： ";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        //控制粗细和颜色
+        Pen blue = new Pen(Color.Blue, 3);
+        Pen red = new Pen(Color.Red, 2);
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th)
+        {
+            if (n == 0) return;
+            //生长点（x1,y1）和（x2,y2）
+            double x1 = x0 + leng *0.5* Math.Cos(th);
+            double y1 = y0 + leng *0.5* Math.Sin(th);
+            double x2 = x0 + leng *1.2* Math.Cos(th-0.2);
+            double y2 = y0 + leng *1.2* Math.Sin(th-0.2);
+
+            drawLine1(x0, y0, x1, y1);
+            drawLine2(x0, y0, x2, y2);
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x2, y2, per2 * leng, th - th2);
+
+        }
+        void drawLine1(double x0,double y0,double x1,double y1)
+        {
+            graphics.DrawLine(blue,(int)x0, (int)y0, (int)x1, (int)y1);
+        }
+        void drawLine2(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(red, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s1 = angle1.Text;
+            double d1 = double.Parse(s1);
+            string s2 = angle2.Text;
+            double d2 = double.Parse(s2);
+            string s3 = length1.Text;
+            double l1 = double.Parse(s3);
+            string s4 = length2.Text;
+            double l2 = double.Parse(s4);
+            th1 = d1 * Math.PI / 180;
+            th1 = d2 * Math.PI / 180;
+            per1 = l1;
+            per2 = l2;
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
+        }
+
+
+        private void vbar_OnValueChanged(object sender, EventArgs e)
+        {
+            //pb.Location = new Point(pb.Left, (pnl.Size.Height) * vbar.Value / (vbar.Maximum - vbar.LargeChange + 1));
+        }
+    }
+}
